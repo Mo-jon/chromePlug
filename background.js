6a3da0bc948a05c5486484309fe846f0aa5b0739
mojon <52648362+Mo-jon@users.noreply.github.com>
2020-05-15 09:29:09 +0800

    项目初始文件上传
    这是一个简单的 chrome 插件入门开发

---
+++ background.js
@@ -0,0 +1,8 @@
+(function (global, factory) {
+    factory();
+    var init = {
+        version: '1.0.0'
+    };
+    console.log('welcome to mojon plug!');
+    global.mojon = init;
+})(this, function () {})
